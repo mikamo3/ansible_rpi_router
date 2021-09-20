c0c03edc3d64a87404157dbe36e0d995aef59d90
mikamo3 <kamo3proj@gmail.com>
2021-09-20 09:12:53 +0000

    :tada: (all) initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+if [[ "$1" != "" ]]; then
+  TAGS="--tags=$1"
+  shift
+fi
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts.yml playbook.yml $TAGS --vault-password-file=".pass" $@
